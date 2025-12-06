5f41b8bd6206f62b3b8d4a8b73473ec9a97be535
Arjun Amar <145699985+arjunamar5@users.noreply.github.com>
2025-12-06 12:20:57 +0530

    Fix import statement for JobPostings_creation.css

--- TaskThozhanA/src/JobPostings_creation.jsx
+++ TaskThozhanA/src/JobPostings_creation.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Jobpostings_creation.css';
+import './JobPostings_creation.css';
 
 const JobPostings_creation = () => {
   const locationData = {
